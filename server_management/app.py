2e1be3cc87962541c5c2151913dd8188a24e6d4f
bikee1235 <bikee.bihari@sstsinc.com>
2025-04-01 20:53:30 +0530

    fix v1

--- server_management/app.py
+++ server_management/app.py
@@ -279,4 +279,4 @@ def export_machines():
         return redirect(url_for('admin_dashboard'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
